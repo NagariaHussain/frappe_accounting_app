381b933aa39f642c487f6fd7b3f5481da935ec61
Hussain <hussainbhaitech@gmail.com>
2021-03-06 00:28:37 +0530

    feat: Add file import logic to COA Importer frontend

--- accounting_app/accounting_app/doctype/coa_importer/coa_importer.js
+++ accounting_app/accounting_app/doctype/coa_importer/coa_importer.js
@@ -1,8 +1,29 @@
 // Copyright (c) 2021, Mohammad Hussain Nagaria and contributors
 // For license information, please see license.txt
 
+frappe.ui.form.on('COA Importer', {
+	onload: function(frm) {
+		frm.set_value("import_file", "");
+	},
+	refresh: function(frm) {
+		frm.disable_save();
+		create_import_button(frm);
+	 }
+});
 
-// frappe.ui.form.on('COA Importer', {
-// 	refresh: function(frm) {
-// 	 }
-// });
+var create_import_button = function(frm) {
+	frm.page.set_primary_action(
+		__("Import"), function() {
+			frappe.call({
+				method: "accounting_app.accounting_app.doctype.coa_importer.coa_importer.import_coa", 
+				args: {
+					filename: frm.doc.import_file
+				},
+				freeze: true,
+				callback: function(r) {console.log(r);}
+			});
+		}
+	);
+};
+
+
