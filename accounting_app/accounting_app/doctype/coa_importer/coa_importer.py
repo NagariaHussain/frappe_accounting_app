691a2d25e74623b3c99d720623771240cd9c7307
Hussain <hussainbhaitech@gmail.com>
2021-03-06 00:26:37 +0530

    feat: Add JSON file upload backend method

--- accounting_app/accounting_app/doctype/coa_importer/coa_importer.py
+++ accounting_app/accounting_app/doctype/coa_importer/coa_importer.py
@@ -6,5 +6,17 @@ from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
 
+@frappe.whitelist()
+def import_coa(filename):
+    # Get the doc having the given filename
+    file_doc = frappe.get_doc("File", {"file_url": filename})
+    
+    # Extract name and extension
+    name, extension = file_doc.get_extension()
+    
+    # If not a json file
+    if extension != ".json":
+        frappe.throw("Please upload a JSON file.")
+
 class COAImporter(Document):
 	pass
