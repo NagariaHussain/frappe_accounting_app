9a53e345c4950c0856c670eea73efce4f518137d
Hussain <hussainbhaitech@gmail.com>
2021-03-06 00:18:02 +0530

    feat: Add Account DocType

---
+++ accounting_app/accounting_app/doctype/account/account.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Mohammad Hussain Nagaria and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Account', {
+	// refresh: function(frm) {
+
+	// }
+});
