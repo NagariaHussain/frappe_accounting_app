ee50fdcc1f1f8a735745bbe7d20b41bf7b1de62d
Hussain <hussainbhaitech@gmail.com>
2021-03-06 00:06:21 +0530

    feat: add Item DocType

---
+++ accounting_app/accounting_app/doctype/item/item.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Mohammad Hussain Nagaria and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Item', {
+	// refresh: function(frm) {
+
+	// }
+});
