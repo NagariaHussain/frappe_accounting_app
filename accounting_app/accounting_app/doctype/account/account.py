e6bcc4318fbf8a7869fa305591b7b783c058ac2c
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-03-08 10:55:49 +0530

    feat: Add autonaming to Account DocType

--- accounting_app/accounting_app/doctype/account/account.py
+++ accounting_app/accounting_app/doctype/account/account.py
@@ -4,7 +4,19 @@
 
 from __future__ import unicode_literals
 # import frappe
+
 from frappe.utils.nestedset import NestedSet
+from frappe.utils import cstr
 
 class Account(NestedSet):
-	pass
+	def autoname(self):
+		self.name = self.get_autoname_with_number(
+			self.account_number,
+			self.account_name
+		)
+	
+	def get_autoname_with_number(self, number_value, doc_title):
+		name = doc_title
+		if number_value:
+			name += (' - ' + number_value)
+		return name 
