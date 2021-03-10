3c100058078f2aa0bc2164175b9cb43e739d66eb
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-03-10 11:57:38 +0530

    Add Journal Entry DocType

---
+++ accounting_app/accounting_app/doctype/journal_entry/journal_entry.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Mohammad Hussain Nagaria and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Journal Entry', {
+	// refresh: function(frm) {
+
+	// }
+});
