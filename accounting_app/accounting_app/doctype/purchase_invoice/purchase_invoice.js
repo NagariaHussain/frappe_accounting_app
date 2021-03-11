f9afff58c87bcb87fe0b87fb899ac818be966833
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-03-11 12:55:51 +0530

    feat: Add Purchase Invoice DocType

---
+++ accounting_app/accounting_app/doctype/purchase_invoice/purchase_invoice.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Mohammad Hussain Nagaria and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Purchase Invoice', {
+	// refresh: function(frm) {
+
+	// }
+});
