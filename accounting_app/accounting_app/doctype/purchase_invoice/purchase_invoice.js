445399fe8bd656f193caa9150edbda0e455fc0c7
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-03-11 13:50:24 +0530

    feat: Add amount auto update on PII grid view

--- accounting_app/accounting_app/doctype/purchase_invoice/purchase_invoice.js
+++ accounting_app/accounting_app/doctype/purchase_invoice/purchase_invoice.js
@@ -1,8 +1,16 @@
 // Copyright (c) 2021, Mohammad Hussain Nagaria and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Purchase Invoice', {
-	// refresh: function(frm) {
-
-	// }
+frappe.ui.form.on('Purchase Invoice Item', {
+	qty(frm, cdt, cdn) {
+		update_amount(cdt, cdn);
+	},
+	rate(frm, cdt, cdn) {
+		update_amount(cdt, cdn);
+	}
 });
+
+function update_amount(cdt, cdn) {
+	let item = frappe.get_doc(cdt, cdn);
+	item.amount = item.qty * item.rate;
+}
