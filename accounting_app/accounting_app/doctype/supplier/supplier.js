07f6f6ed5362260894739290b087a1fad274c7af
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-03-11 12:50:03 +0530

    feat: Add Supplier DocType

---
+++ accounting_app/accounting_app/doctype/supplier/supplier.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Mohammad Hussain Nagaria and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Supplier', {
+	// refresh: function(frm) {
+
+	// }
+});
