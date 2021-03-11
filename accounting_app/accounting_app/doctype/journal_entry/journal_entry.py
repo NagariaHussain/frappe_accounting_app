f2c8b16f9814a35a8ec189ee8c489a93cce38517
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-03-11 07:23:30 +0530

    feat: Add Journal Entry balance validation

--- accounting_app/accounting_app/doctype/journal_entry/journal_entry.py
+++ accounting_app/accounting_app/doctype/journal_entry/journal_entry.py
@@ -3,8 +3,33 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.utils import flt
 
 class JournalEntry(Document):
-	pass
+	def validate(self):
+		self.validate_total_debit_credit()
+	
+	def validate_total_debit_credit(self):
+		'''throw if total credit not equal to total debit'''
+		total_debit, total_credit = 0, 0
+		difference = 0
+
+		for acc in self.get("accounts"):
+			total_debit = flt(total_debit) + flt(acc.debit, acc.precision("debit"))
+			total_credit = flt(total_credit) + flt(acc.credit, acc.precision("credit"))
+		
+		difference = total_debit - total_credit
+
+		if difference:
+			frappe.throw(frappe._("Total Debit must be equal to Total Credit. The difference is {0}"
+							.format(difference)
+			))
+
+	def on_submit(self):
+		self.make_gl_entries()
+
+	def make_gl_entries(self):
+		'''create and save General Ledger Entries'''
+		pass
