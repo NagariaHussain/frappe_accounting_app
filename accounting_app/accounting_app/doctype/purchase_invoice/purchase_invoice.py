078574e93cb0dfda5c86db921bd2993fc9587fcd
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-03-12 16:26:45 +0530

    feat: Add PI status update logic

--- accounting_app/accounting_app/doctype/purchase_invoice/purchase_invoice.py
+++ accounting_app/accounting_app/doctype/purchase_invoice/purchase_invoice.py
@@ -7,4 +7,24 @@ from __future__ import unicode_literals
 from frappe.model.document import Document
 
 class PurchaseInvoice(Document):
-	pass
+	def validate(self):
+		self.set_status()
+		
+	def set_status(self):
+		if self.is_new():
+			if self.get('amended_form'):
+				self.status = "Draft"
+			return
+		
+		if self.docstatus == 2:
+			self.status = "Cancelled"
+		elif self.docstatus == 1:
+			if self.paid:
+				self.status = "Paid"
+			else:
+				self.status = "Unpaid"
+
+
+
+
+	
