8be05fab4c216396dc6e650a21dd4dbc2af00510
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-03-16 13:56:37 +0530

    feat: Add create payment entry button to frontend

--- accounting_app/accounting_app/doctype/purchase_invoice/purchase_invoice.js
+++ accounting_app/accounting_app/doctype/purchase_invoice/purchase_invoice.js
@@ -1,6 +1,32 @@
 // Copyright (c) 2021, Mohammad Hussain Nagaria and contributors
 // For license information, please see license.txt
 
+frappe.ui.form.on('Purchase Invoice', {
+	refresh: (doc) => {
+		if (!doc.paid) {
+			this.frm.add_custom_button(
+				__('Payment'), 
+				(frm) => { 
+					return frappe.call({
+						method: "accounting_app.accounting_app.doctype.payment_entry.payment_entry.get_payment_entry",
+						args: {
+							dt: this.frm.doc.doctype,
+							dn: this.frm.doc.name
+						},
+						callback: function(r) {
+							var doc = frappe.model.sync(r.message);
+							frappe.set_route("Form", doc[0].doctype, doc[0].name);
+						}
+					});
+				}, 
+				__('Create')
+			);
+		} else {
+			console.log("Paid");
+		}
+	}
+});
+
 frappe.ui.form.on('Purchase Invoice Item', {
 	qty(frm, cdt, cdn) {
 		update_amount(cdt, cdn);
