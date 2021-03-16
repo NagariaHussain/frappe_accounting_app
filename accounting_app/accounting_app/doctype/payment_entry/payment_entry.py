ec2804d23e58890a0d9472fe7ff4ac0f6cbca77f
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-03-16 13:40:18 +0530

    feat: Add get_payment_entry to backend

--- accounting_app/accounting_app/doctype/payment_entry/payment_entry.py
+++ accounting_app/accounting_app/doctype/payment_entry/payment_entry.py
@@ -3,8 +3,24 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.utils import nowdate
 
 class PaymentEntry(Document):
 	pass
+
+@frappe.whitelist()
+def get_payment_entry(dt, dn):
+	# Fetch the associated doc
+	doc = frappe.get_doc(dt, dn)
+
+	# Create new payment entry
+	pe = frappe.new_doc("Payment Entry")
+	pe.amount = doc.grand_total
+	pe.voucher_type = doc.doctype
+	pe.voucher_link = doc.name
+	pe.posting_date = nowdate()
+
+	# return pe doc
+	return pe
