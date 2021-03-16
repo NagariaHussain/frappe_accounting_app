a949f539a969c9e44ffd6d2879cfae668bfdce0a
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-03-16 17:03:50 +0530

    feat: Add Sales Invoice DocType

---
+++ accounting_app/accounting_app/doctype/sales_invoice/sales_invoice.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Mohammad Hussain Nagaria and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Sales Invoice', {
+	// refresh: function(frm) {
+
+	// }
+});
