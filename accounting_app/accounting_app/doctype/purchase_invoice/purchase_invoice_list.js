937af9d8254f493ba15b511f3ba3c28979d73a70
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-03-16 16:55:26 +0530

    fix: Status indicator on PI list page

--- accounting_app/accounting_app/doctype/purchase_invoice/purchase_invoice_list.js
+++ accounting_app/accounting_app/doctype/purchase_invoice/purchase_invoice_list.js
@@ -1,8 +1,12 @@
 frappe.listview_settings['Purchase Invoice'] = {
     get_indicator: function(doc) {
-        if (doc.paid) {
+        if (doc.status == "Paid") {
+            console.log(doc, "is paid");
             return [__("Paid"), "green"];
-        } else {
+        } 
+        else if (doc.status == "Unpaid") 
+        {
+            console.log("not paid");
             return [__("Unpaid"), "darkgrey"];
         }
     }
