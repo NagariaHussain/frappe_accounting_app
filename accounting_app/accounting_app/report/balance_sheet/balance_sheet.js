f18ab6370f42915440008db2e6bd716c852ea412
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-03-17 12:32:21 +0530

    feat: Add Report Balance Sheet

---
+++ accounting_app/accounting_app/report/balance_sheet/balance_sheet.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2016, Mohammad Hussain Nagaria and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Balance Sheet"] = {
+	"filters": [
+
+	]
+};
