fa47cbabb9f9c6424b9e387b20fc3d49609fbb4a
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-03-17 16:32:33 +0530

    feat: Add get_account function to BS report

--- accounting_app/accounting_app/report/balance_sheet/balance_sheet.py
+++ accounting_app/accounting_app/report/balance_sheet/balance_sheet.py
@@ -2,8 +2,14 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 
 def execute(filters=None):
 	columns, data = [], []
 	return columns, data
+
+def get_accounts(root_type):
+	return frappe.db.sql("""
+	select name, account_number, parent_account, lft, rgt, root_type, report_type, account_name, account_type, is_group, lft, rgt
+	from tabAccount
+	where root_type=%s order by lft""", (root_type), as_dict=True)
