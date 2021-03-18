db210d698853986480a44a5883675edec1d9347e
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-03-18 20:39:40 +0530

    feat: Add Balance Sheet data generation

--- accounting_app/accounting_app/report/balance_sheet/balance_sheet.py
+++ accounting_app/accounting_app/report/balance_sheet/balance_sheet.py
@@ -2,15 +2,81 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
+
 import frappe
 import functools
 import re
+
 from past.builtins import cmp
-from frappe.utils import getdate
+from frappe.utils import getdate, flt
+from six import itervalues
 
 def execute(filters=None):
 	columns, data = [], []
+
+	# Get data by root account type
+	asset = get_data("Asset", "Debit", filters.from_date, filters.to_date)
+	liability = get_data("Liability", "Debit", filters.from_date, filters.to_date)
+	equity = get_data("Equity", "Credit", filters.from_date, filters.to_date)
+	
+	# Add data to rows
+	data.extend(asset or [])
+	data.extend(liability or [])
+	data.extend(equity or [])
+
+	# Add columns
+	columns = get_columns()
+
 	return columns, data
+
+def get_columns():
+	columns = [{
+		"fieldname": "account",
+		"label": frappe._("Account"),
+		"fieldtype": "Link",
+		"options": "Account",
+		"width": 300
+	}, 
+	{
+		"fieldname": "to_date",
+		"label": "Amount",
+		"fieldtype": "Currency",
+		"options": "currency",
+		"width": 150
+	}]
+
+	return columns
+
+def get_data(root_type, balance_must_be, from_date, to_date, total = True):
+	accounts = get_accounts(root_type)
+
+	if not accounts:
+		return None
+
+	accounts, accounts_by_name, parent_children_map = filter_accounts(accounts)
+
+	gl_entries_by_account = {}
+	for root in frappe.db.sql("""select lft, rgt from tabAccount
+			where root_type=%s and ifnull(parent_account, '') = ''""", root_type, as_dict=1):
+
+		set_gl_entries_by_account(
+			gl_entries_by_account,
+			from_date,
+			to_date,
+			root.lft, root.rgt)
+	
+	calculate_values(
+		accounts_by_name, gl_entries_by_account)
+	accumulate_values_into_parents(accounts, accounts_by_name)
+
+	out = prepare_data(accounts, balance_must_be, from_date, to_date)
+	print("out1", out)
+	# out = filter_out_zero_value_rows(out, parent_children_map)
+	print("out2", out)
+	if out and total:
+		add_total_row(out, root_type, balance_must_be)
+
+	return out
 
 def get_accounts(root_type):
 	return frappe.db.sql("""
@@ -86,3 +152,74 @@ def set_gl_entries_by_account(gl_entries_by_account, from_date, to_date, root_lf
 			gl_entries_by_account.setdefault(entry.account, []).append(entry)
 
 	return gl_entries_by_account
+
+def calculate_values(accounts_by_name, gl_entries_by_account):
+	for entries in itervalues(gl_entries_by_account):
+		for entry in entries:
+			d = accounts_by_name.get(entry.account)
+			d["opening_balance"] = d.get("opening_balance", 0.0) + flt(entry.debit) - flt(entry.credit)
+
+def accumulate_values_into_parents(accounts, accounts_by_name):
+	"""accumulate children's values in parent accounts"""
+	for d in reversed(accounts):
+		if d.parent_account:
+			accounts_by_name[d.parent_account]["opening_balance"] = \
+				accounts_by_name[d.parent_account].get("opening_balance", 0.0) + d.get("opening_balance", 0.0)
+
+def prepare_data(accounts, balance_must_be, from_date, to_date):
+	data = []
+	year_start_date = from_date
+	year_end_date = to_date
+
+	for d in accounts:
+		# add to output
+		has_value = False
+		total = 0
+		row = frappe._dict({
+			"account": frappe._(d.name),
+			"parent_account": frappe._(d.parent_account) if d.parent_account else '',
+			"indent": flt(d.indent),
+			"year_start_date": year_start_date,
+			"year_end_date": year_end_date,
+			"account_type": d.account_type,
+			"is_group": d.is_group,
+			"opening_balance": d.get("opening_balance", 0.0) * (1 if balance_must_be=="Debit" else -1),
+			"account_name": ('%s - %s' %(frappe._(d.account_number), frappe._(d.account_name))
+				if d.account_number else frappe._(d.account_name))
+		})
+
+		
+		row["to_date"] = flt(d.get("to_date", 0.0), 3)
+
+		if abs(row["to_date"]) >= 0.005:
+			# ignore zero values
+			has_value = True
+			total += flt(row["to_date"])
+
+		row["has_value"] = has_value
+		row["total"] = total
+		data.append(row)
+
+	return data
+
+def add_total_row(out, root_type, balance_must_be):
+	total_row = {
+		"account_name": frappe._("Total {0} ({1})").format(frappe._(root_type), frappe._(balance_must_be)),
+		"account": frappe._("Total {0} ({1})").format(frappe._(root_type), frappe._(balance_must_be))
+	}
+
+	for row in out:
+		if not row.get("parent_account"):
+			total_row.setdefault('to_date', 0.0)
+			total_row['to_date'] += row.get('to_date', 0.0)
+			row['to_date'] = row.get('to_date', 0.0)
+
+			total_row.setdefault("total", 0.0)
+			total_row["total"] += flt(row["total"])
+			row["total"] = ""
+
+	if "total" in total_row:
+		out.append(total_row)
+
+		# blank row after Total
+		out.append({})
