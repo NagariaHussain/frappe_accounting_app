173e1402a3df46db2fb79cbfc94c78cfbd2e4f93
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-03-19 19:05:33 +0530

    feat: Add P&L Statement generation

--- accounting_app/accounting_app/report/profit_and_loss_statement/profit_and_loss_statement.py
+++ accounting_app/accounting_app/report/profit_and_loss_statement/profit_and_loss_statement.py
@@ -2,8 +2,39 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
+from accounting_app.accounting_app.report.financial_statements import get_data, get_columns
+from frappe.utils import flt
 
 def execute(filters=None):
 	columns, data = [], []
+
+	income = get_data("Income", "Credit", filters.from_date, filters.to_date)
+	expense = get_data("Expense", "Debit", filters.from_date, filters.to_date)
+
+	net_profit_loss = get_net_profit_loss(income, expense)
+
+	data.extend(income or [])
+	data.extend(expense or [])
+
+	if net_profit_loss:
+		data.append(net_profit_loss)
+	
+	columns = get_columns()
+
 	return columns, data
+
+
+def get_net_profit_loss(income, expense):
+	net_profit_loss = {
+		"account_name": "'" + frappe._("Profit for the year") + "'",
+		"account": "'" + frappe._("Profit for the year") + "'",
+		"warn_if_negative": True
+	}
+
+	total_income = flt(income[-2]['opening_balance'], 3)
+	total_expense = flt(expense[-2]['opening_balance'], 3)
+
+	net_profit_loss['opening_balance'] = total_income - total_expense
+
+	return net_profit_loss
