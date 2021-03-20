6104f673c86fdbbb946fbb75675ffdca4e8136c6
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-03-20 13:42:29 +0530

    feat: Add add/remove to cart functionality

--- accounting_app/www/catalog.js
+++ accounting_app/www/catalog.js
@@ -1,6 +1,9 @@
 // Get all add to cart buttons
 const add_to_cart_buttons = document.querySelectorAll(".atc-button");
 
+// To store cart data
+let cart = [];
+
 // Add click listeners to all ATC buttons
 for (let button of add_to_cart_buttons) {
     button.addEventListener('click', atcButtonListener);
@@ -8,8 +11,25 @@ for (let button of add_to_cart_buttons) {
 
 // Add to cart event callback
 function atcButtonListener(event) {
-    console.log(
-        event.target.getAttribute("data-name"), 
-        "will be added to the cart!"
-    );
+    const atcButton = event.target;
+    const itemName = atcButton.getAttribute("data-name");
+
+    if (atcButton.hasAttribute('data-added')) {
+        // Item already added to cart, remove it
+        const itemIndex = cart.indexOf(itemName);
+        cart.splice(itemIndex, 1);
+        // Remove the added attribute
+        atcButton.removeAttribute('data-added');
+        // Change button style and text
+        atcButton.innerText = 'Add to cart';
+        atcButton.classList.replace('btn-danger', 'btn-success');
+    } else {
+        // Add this item to cart
+        cart.push(itemName);
+        // Add the added attribute
+        atcButton.setAttribute('data-added', '');
+        // Change button style and text
+        atcButton.innerText = 'Remove';
+        atcButton.classList.replace('btn-success', 'btn-danger');
+    }
 }
