6a6f49030759ec4a0e9ba5a5b5dd97ba18bc41e5
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-03-26 15:33:47 +0530

    test: Add test for payment_entry.get_payment_entry function

--- accounting_app/accounting_app/doctype/payment_entry/test_payment_entry.py
+++ accounting_app/accounting_app/doctype/payment_entry/test_payment_entry.py
@@ -3,8 +3,40 @@
 # See license.txt
 from __future__ import unicode_literals
 
-# import frappe
+import frappe
 import unittest
 
+from accounting_app.accounting_app.doctype.payment_entry.payment_entry import get_payment_entry
+
 class TestPaymentEntry(unittest.TestCase):
-	pass
+	def test_get_payment_entry_for_pi(self):
+		pi = get_sample_pi()
+
+		pe = get_payment_entry('Purchase Invoice', pi.name)
+
+		self.assertEqual(pe.amount, pi.grand_total, "PE's amount and PI's grand_total does not match")
+		self.assertEqual(pe.voucher_type, 'Purchase Invoice', "Voucher type should be PI")
+		self.assertEqual(pe.voucher_link, pi.name, "PI name does not match with PE's linked name")
+
+def get_sample_pi():
+	pi = frappe.new_doc('Purchase Invoice')
+	pi.title = "Test Purchase Invoice"
+
+	# Add sample items to child table
+	pi.append("items", {
+			"item": "Envy 13",
+			"qty": 10,
+			"rate": 100,
+			"amount": 1000
+		})
+
+	pi.append("items", {
+			"item": "iPhone 12",
+			"qty": 5,
+			"rate": 10,
+			"amount": 50
+		})
+
+	pi.insert()
+
+	return pi
